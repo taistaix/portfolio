7c6a6608f5f3a503d26abfe5370b1be943a51bde
Taís <77944435+taistaix@users.noreply.github.com>
2022-12-07 07:07:33 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function openNav() {
+
+    var x = document.getElementById("navigation");
+
+    if (x. className === "navigation") {
+        x.className == "menujs";
+  } else {
+    x.className = "navigation";
+    
+
+
+  }
+
+
+}
